afe0e03988103706dd88d0eccb9ecafb7308bdc6
AANikolaeva <aadeg147@gmail.com>
2021-07-08 13:12:44 +0600

    Задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,10 +11,27 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+// Пункт задания 3
+//		Point p1 = new Point(0,0 );
+//		Point p2 = new Point(3, 4);
+//		System.out.println("Расстояние между двумя точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y +
+//						" на плостости равно " + distance(p1, p2));
+
+// Пункт задания 4
+		Point p1 = new Point(0,0 );
+		Point p2 = new Point(3, 4);
+		System.out.println("Расстояние между двумя точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y +
+						" на плостости равно " + p1.distance(p2));
+
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
+// Пункт задания 2
+//	public static double distance(Point p1, Point p2){
+//		return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+//	}
 
 }
