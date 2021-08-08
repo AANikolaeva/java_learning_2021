745830e6e86b28c212085e63d422b9773b0c9040
AANikolaeva <aadeg147@gmail.com>
2021-08-08 18:13:00 +0600

    Fluent-интерфейсы для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,20 +14,21 @@ public class GroupModificationTests extends TestBase {
  public void ensurePreconditions() {
   app.goTo().groupPage();
   if (app.group().list().size() == 0) {
-   app.group().create(new GroupData("test_0", null, null));
+   app.group().create(new GroupData().withName("test1"));
   }
  }
 
  @Test
   public void testGroupModification() {
    List<GroupData> before = app.group().list();
-   int indexGroup = before.size() - 1;
-   GroupData group = new GroupData(before.get(indexGroup).getId(),"test_0", "test_1", "test_2");
-   app.group().modify(indexGroup, group);
+   int index = before.size() - 1;
+   GroupData group = new GroupData()
+           .withId(before.get(index).getId()).withName("test_0").withHeader("test_1").withFooter("test_2");
+   app.group().modify(index, group);
    List<GroupData> after = app.group().list();
    Assert.assertEquals(after.size(), before.size());
 
-   before.remove(indexGroup);
+   before.remove(index);
    before.add(group);
    Comparator<? super GroupData> byID = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
    before.sort(byID);
